845c8689ab50b0c359c6a4b75d98066f7bb39ed9
Cyril <github@kosmon.fr>
2022-12-02 13:36:46 +0100

    Add timing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::time;
 
 pub mod day_01;
 pub mod day_02;
@@ -6,6 +7,7 @@ pub mod day_02;
 fn main() {
     println!("Welcome to Advent of Code 2022");
     let args: Vec<String> = env::args().collect();
+    let mut times: Vec<f64> = Vec::new();
     let mut day = 0;
     if args.len() == 2 {
         println!("Running only day {}", args[1]);
@@ -18,12 +20,20 @@ fn main() {
     }
 
     if day == 0 || day == 1 {
+        let now = time::Instant::now();
         day_01::solve_all();
+        let t = now.elapsed().as_secs_f64();
+        println!("Day 1 solved in {:.3}s", t);
+        times.push(t);
         println!("----------");
     }
 
     if day == 0 || day == 2 {
+        let now = time::Instant::now();
         day_02::solve_all();
+        let t = now.elapsed().as_secs_f64();
+        println!("Day 2 solved in {:.3}s", t);
+        times.push(t);
         println!("----------");
     }
 }
