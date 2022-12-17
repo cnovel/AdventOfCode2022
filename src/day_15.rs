d4520dd1989047b6102b6dcade156154c7474f6d
Cyril <github@kosmon.fr>
2022-12-17 09:47:23 +0100

    Day 15 - Part 2

--- src/day_15.rs
+++ src/day_15.rs
@@ -1,6 +1,40 @@
 use std::{fs, collections::HashSet};
 use regex::Regex;
 
+fn manhattan(a: &(i32,i32), b: &(i32, i32)) -> i32 {
+    return (a.0 - b.0).abs() + (a.1 - b.1).abs();
+}
+
+fn find_beacon(sensors: &Vec<(i32, i32)>, man_dist: &Vec<i32>, y: i32) {
+    for i in 0..sensors.len() {
+        let si = sensors[i];
+        let di = man_dist[i];
+        for diff_x in 0..di+2 {
+            let diff_y = di+1 - diff_x;
+            let pos = [(si.0-diff_x, si.1-diff_y), (si.0-diff_x, si.1+diff_y),
+                (si.0+diff_x, si.1-diff_y), (si.0+diff_x, si.1+diff_y)];
+            for p in &pos {
+                let mut seen = false;
+                for j in 0..sensors.len() {
+                    if i == j {
+                        continue;
+                    }
+
+                    if p.0 < 0 || p.1 < 0 || p.0 > y*2 || p.1 > y*2 || manhattan(p, &sensors[j]) <= man_dist[j] {
+                        seen = true;
+                        break;
+                    }
+                }
+
+                if !seen {
+                    println!("Score 2 = {} ({},{})", p.0 as i128 * 4000000 as i128 + p.1 as i128, p.0, p.1);
+                    return;
+                }
+            }
+        }
+    }
+}
+
 fn solve(p: &str, s: &str, y: i32) {
     println!("Solving Day 15 - {}", s);
 
@@ -36,6 +70,8 @@ fn solve(p: &str, s: &str, y: i32) {
     }
 
     println!("Score 1 = {}", impossible_beacons.len());
+
+    find_beacon(&sensors, &man_dist, y);
 }
 
 pub fn solve_all() {
